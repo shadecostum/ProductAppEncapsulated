160ab63e5e90ef9b251688099a84435178368218
Akash santhosh <akash777kk@gmail.com>
2023-08-11 23:01:49 +0530

    image

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace ProductAppVersion2
     {
         static void Main(string[] args)
         {
-            Product product1 = new Product(1, "akash", 1000, 50.0);
+            Product product1 = new Product(1, "Bag", 1000, 50.0);
             Console.WriteLine(product1.TotalPrice());
-            Product product2 = new Product(2, "Akshay", 600);
+            Product product2 = new Product(2, "Shirt", 600);
             Console.WriteLine(product2.TotalPrice());
         }
     }
